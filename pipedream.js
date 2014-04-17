ee8230b82a4c26889e21c77f6dcb30a6c2ea9431
Bernard Wolff <bernard.wolff@tntp.org>
2014-04-17 18:09:29 -0400

    rearranged some stuff

--- pipedream.js
+++ pipedream.js
@@ -1,4 +1,4 @@
-function Pipedream(opts) {
+function PipedreamEngine(opts) {
   this.background_color = opts.background_color || "gray";
   this.grid_color = opts.grid_color || "white";
   this.pipe_color = opts.pipe_color || "black";
@@ -14,65 +14,71 @@ function Pipedream(opts) {
   var $canvas = $("#" + opts.canvas_id);
   var canvas = $canvas[0];
   var context = canvas.getContext("2d");
+  this.ctx = context;
 
   $canvas.attr({ width: this.width, height: this.height });
 
+  var clear_screen = (function clear_screen() {
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, this.width, this.height);
+    this.ctx.fillStyle = this.background_color;
+    this.ctx.fill();
+  }).bind(this);
+
   var draw_grid = (function draw_grid() {
-    context.beginPath();
-    context.rect(0, 0, this.width, this.height);
-    context.fillStyle = this.background_color;
-    context.fill();
     for (var i = 0; i < this.grid_width; i++) {
       for (var j = 0; j < this.grid_height; j++) {
         var x = i * this.cell_width;
         var y = j * this.cell_height;
-        context.beginPath();
-        context.strokeStyle = this.grid_color;
-        context.lineWidth = this.grid_line_width;
-        context.rect(x, y, this.cell_width, this.cell_height);
-        context.stroke();
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.grid_color;
+        this.ctx.lineWidth = this.grid_line_width;
+        this.ctx.rect(x, y, this.cell_width, this.cell_height);
+        this.ctx.stroke();
       }
     }
   }).bind(this);
-
-  var draw_animated_line = (function draw_animated_line() {
-    // put some sleeps between the strokes
+  
+  var draw_straight_pipe = (function draw_straight_pipe() {
     for (var x_offset = 0; x_offset < this.cell_width / 2 - this.pipe_width / 2; x_offset++) {
-      context.save();
-      context.strokeStyle = this.pipe_color;
-      context.beginPath();
-      context.translate(0 + x_offset, this.cell_height / 2 + this.pipe_width / 2);
-      context.lineWidth = 1;
-      context.moveTo(0, 0);
-      context.lineTo(0, -1 * this.pipe_width);
-      context.stroke();
-      context.restore();
+      this.ctx.save();
+      this.ctx.strokeStyle = this.pipe_color;
+      this.ctx.beginPath();
+      this.ctx.translate(0 + x_offset, this.cell_height / 2 + this.pipe_width / 2);
+      this.ctx.lineWidth = 1;
+      this.ctx.moveTo(0, 0);
+      this.ctx.lineTo(0, -1 * this.pipe_width);
+      this.ctx.stroke();
+      this.ctx.restore();
     }
   }).bind(this);
 
   var draw_elbow = (function draw_elbow() {
-    draw_animated_line();
-    
-    for (var degrees = 0; degrees < 90; degrees++) {
-      context.save();
-      context.beginPath();
-      context.translate(this.cell_width / 2 - this.pipe_width / 2, this.cell_height / 2 + this.pipe_width / 2);
-      context.rotate(degrees * Math.PI / 180);
-      context.strokeStyle = this.pipe_color;
-      context.lineWidth = 1;
-      context.moveTo(0, 0);
-      context.lineTo(0, -1 * this.pipe_width);
-      context.stroke();
-      context.restore();
-    }
+      draw_straight_pipe();
+      for (var degrees = 0; degrees < 90; degrees++) {
+        this.ctx.save();
+        this.ctx.strokeStyle = this.pipe_color;
+        this.ctx.beginPath();
+        this.ctx.translate(this.cell_width / 2 - this.pipe_width / 2, this.cell_height / 2 + this.pipe_width / 2);
+        this.ctx.rotate(degrees * Math.PI / 180);
+        this.ctx.lineWidth = 1;
+        this.ctx.moveTo(0, 0);
+        this.ctx.lineTo(0, -1 * this.pipe_width);
+        this.ctx.stroke();
+        this.ctx.restore();
+      }
 
-    context.save();
-    context.translate(this.cell_width, this.cell_height / 2 + this.pipe_width / 2);
-    context.rotate(90 * Math.PI / 180);
-    draw_animated_line();
-    context.restore();
+      this.ctx.save();
+      this.ctx.translate(this.cell_width, this.cell_height / 2 + this.pipe_width / 2);
+      this.ctx.rotate(90 * Math.PI / 180);
+      draw_straight_pipe();
+      this.ctx.restore();
   }).bind(this);
 
-  draw_grid();
+  clear_screen();
   draw_elbow();
+  draw_grid();
 }
+
+
+
